7426e2136d44cf71c75a9616fea7e66f1c8f5eab
Jorge Luis <jorjoluiso@gmail.com>
2023-05-06 14:30:55 -0500

    [ADD] User and role relation

---
+++ chapter_vi/overlord.js/src/models/userrole.js
@@ -0,0 +1,31 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class UserRole extends Model {
+    static associate (models) {
+      UserRole.belongsTo(models.User, {
+        targetKey: 'id',
+        foreignKey: 'user_id',
+        as: 'user'
+      })
+      UserRole.belongsTo(models.Role, {
+        targetKey: 'name',
+        foreignKey: 'role',
+        as: 'role'
+      })
+    }
+  }
+  UserRole.init({
+    userId: DataTypes.INTEGER,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'UserRole',
+    underscored: true,
+    timestamps: false,
+    tableName: 'adm_user_roles'
+  })
+  return UserRole
+}
